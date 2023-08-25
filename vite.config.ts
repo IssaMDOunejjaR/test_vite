6d950f3a3228ffcd7ffda4f473b255602dbbf60a
IssaMDOunejjaR <otaku6issam@gmail.com>
2023-08-25 17:29:31 +0100

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-    const config = {
-        plugins: [react()],
-        base: "/",
-    };
-
-    if (command !== "serve") {
-        config.base = "/test_vite/";
-    }
-
-    return config;
+export default defineConfig({
+    base: "/test_vite/",
+    plugins: [react()],
 });
